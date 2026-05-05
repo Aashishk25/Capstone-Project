62a7afcdf5f435d8ec96a49a9e4edb16a160997e
Aashishk25 <aashish.r.careers@gmail.com>
2026-05-05 15:12:11 +0530

    Create main.tf

---
+++ Task-1-Infrastructure/azure/main.tf
@@ -0,0 +1,64 @@
+resource "azurerm_resource_group" "rg" {
+  name = "capstone-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "capstone-vnet"
+  address_space = ["10.101.0.0/16"]
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "subnet1"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.101.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name = "public-ip"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = "nic"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.subnet.id
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name = "azure-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  size = "Standard_B1s"
+  admin_username = "azureuser"
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer = "UbuntuServer"
+    sku = "18.04-LTS"
+    version = "latest"
+  }
+}
