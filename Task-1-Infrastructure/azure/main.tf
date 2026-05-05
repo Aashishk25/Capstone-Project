d923ec9f1418a8daa3069c875c1f42533511307f
Aashishk25 <aashish.r.careers@gmail.com>
2026-05-05 15:26:44 +0530

    main.tf

--- Task-1-Infrastructure/azure/main.tf
+++ Task-1-Infrastructure/azure/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name = "capstone-rg"
   location = "East US"
@@ -17,6 +21,34 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes = ["10.101.1.0/24"]
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name = "capstone-nsg"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name = "SSH"
+    priority = 1001
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name = "HTTP"
+    priority = 1002
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    destination_port_range = "80"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_public_ip" "pip" {
   name = "public-ip"
   location = azurerm_resource_group.rg.location
@@ -34,6 +66,11 @@ resource "azurerm_network_interface" "nic" {
     subnet_id = azurerm_subnet.subnet.id
     public_ip_address_id = azurerm_public_ip.pip.id
   }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
