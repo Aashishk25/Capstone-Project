2faaeb22e615acc64f1430a3c31b080129cffce7
Aashishk25 <aashish.r.careers@gmail.com>
2026-05-05 15:39:16 +0530

    main.tf

--- Task-1-Infrastructure/aws/main.tf
+++ Task-1-Infrastructure/aws/main.tf
@@ -2,73 +2,93 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# 🔹 Get latest Ubuntu AMI dynamically
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 # VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.100.0.0/16"
 }
 
-# Subnets
+# Public Subnets
 resource "aws_subnet" "public1" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.100.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.100.1.0/24"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public2" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.100.2.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.100.2.0/24"
   map_public_ip_on_launch = true
 }
 
+# Private Subnet
 resource "aws_subnet" "private1" {
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.100.3.0/24"
 }
 
-# IGW
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# NAT
+# Elastic IP for NAT
 resource "aws_eip" "nat" {
   domain = "vpc"
 }
 
+# NAT Gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.public1.id
+  subnet_id     = aws_subnet.public1.id
+
   depends_on = [aws_internet_gateway.igw]
 }
 
-# Route Tables
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route" "public_route" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "pub1" {
-  subnet_id = aws_subnet.public1.id
+  subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table_association" "pub2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Route Table
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route" "private_route" {
-  route_table_id = aws_route_table.private.id
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat.id
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
 resource "aws_route_table_association" "priv1" {
-  subnet_id = aws_subnet.private1.id
+  subnet_id      = aws_subnet.private1.id
   route_table_id = aws_route_table.private.id
 }
 
@@ -77,46 +97,55 @@ resource "aws_security_group" "web" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 # Key Pair
 resource "aws_key_pair" "key" {
-  key_name = "capstone-key"
+  key_name   = "capstone-key"
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-# EC2
+# EC2 - App Machine
 resource "aws_instance" "app" {
-  ami = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public1.id
-  vpc_security_group_ids = [aws_security_group.web.id]
-  key_name = aws_key_pair.key.key_name
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public1.id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  key_name                    = aws_key_pair.key.key_name
+
+  tags = {
+    Name = "App-Machine"
+  }
 }
 
+# EC2 - Tools Machine
 resource "aws_instance" "tools" {
-  ami = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public2.id
-  vpc_security_group_ids = [aws_security_group.web.id]
-  key_name = aws_key_pair.key.key_name
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public2.id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  key_name                    = aws_key_pair.key.key_name
+
+  tags = {
+    Name = "Tools-Machine"
+  }
 }
