2863ede710ebe79a5c10d60927735e98c5e133b3
Aashishk25 <aashish.r.careers@gmail.com>
2026-05-05 17:17:56 +0530

    Update variables.tf

--- Task-1-Infrastructure/azure/variables.tf
+++ Task-1-Infrastructure/azure/variables.tf
@@ -1,1 +1,19 @@
+variable "location" {
+  default = "East US"
+}
 
+variable "vnet_cidr" {
+  default = "10.101.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.101.1.0/24"
+}
+
+variable "vm_size" {
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
