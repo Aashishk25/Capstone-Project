0669c13f9c4f527d92e9877a7abb037745704547
Aashishk25 <aashish.r.careers@gmail.com>
2026-05-05 17:13:27 +0530

    variables.tf

--- Task-1-Infrastructure/aws/variables.tf
+++ Task-1-Infrastructure/aws/variables.tf
@@ -1,1 +1,30 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
 
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  default     = "10.100.0.0/16"
+}
+
+variable "public_subnet_1" {
+  default = "10.100.1.0/24"
+}
+
+variable "public_subnet_2" {
+  default = "10.100.2.0/24"
+}
+
+variable "private_subnet_1" {
+  default = "10.100.3.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key name"
+  default     = "capstone-key"
+}
