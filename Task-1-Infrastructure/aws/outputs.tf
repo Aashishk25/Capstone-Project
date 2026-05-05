07d6edb16e493ec66f65c6c1e4251f378b12cd29
Aashishk25 <aashish.r.careers@gmail.com>
2026-05-05 15:28:11 +0530

    outputs.tf

--- Task-1-Infrastructure/aws/outputs.tf
+++ Task-1-Infrastructure/aws/outputs.tf
@@ -5,10 +5,3 @@ output "aws_app_ip" {
 output "aws_tools_ip" {
   value = aws_instance.tools.public_ip
 }
-output "aws_app_ip" {
-  value = aws_instance.app.public_ip
-}
-
-output "aws_tools_ip" {
-  value = aws_instance.tools.public_ip
-}
