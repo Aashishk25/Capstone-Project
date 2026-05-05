50336f6e7ab892fe6c32c5f38fec11ddf0128d93
Aashishk25 <aashish.r.careers@gmail.com>
2026-05-05 17:19:29 +0530

    Update main.tf

--- Task-1-Infrastructure/azure/main.tf
+++ Task-1-Infrastructure/azure/main.tf
@@ -2,100 +2,133 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name = "capstone-rg"
-  location = "East US"
+  name     = "capstone-rg"
+  location = var.location
+
+  tags = {
+    Environment = "Capstone"
+  }
 }
 
+# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name = "capstone-vnet"
-  address_space = ["10.101.0.0/16"]
-  location = azurerm_resource_group.rg.location
+  name                = "capstone-vnet"
+  address_space       = [var.vnet_cidr]
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Environment = "Capstone"
+  }
 }
 
+# Subnet
 resource "azurerm_subnet" "subnet" {
-  name = "subnet1"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = ["10.101.1.0/24"]
+  address_prefixes     = [var.subnet_cidr]
 }
 
+# Network Security Group
 resource "azurerm_network_security_group" "nsg" {
-  name = "capstone-nsg"
-  location = azurerm_resource_group.rg.location
+  name                = "capstone-nsg"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name = "SSH"
-    priority = 1001
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    destination_port_range = "22"
-    source_address_prefix = "*"
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name = "HTTP"
-    priority = 1002
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    destination_port_range = "80"
-    source_address_prefix = "*"
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
+  }
+
+  tags = {
+    Environment = "Capstone"
   }
 }
 
+# Public IP
 resource "azurerm_public_ip" "pip" {
-  name = "public-ip"
-  location = azurerm_resource_group.rg.location
+  name                = "capstone-public-ip"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method = "Dynamic"
+  allocation_method   = "Dynamic"
+
+  tags = {
+    Environment = "Capstone"
+  }
 }
 
+# Network Interface
 resource "azurerm_network_interface" "nic" {
-  name = "nic"
-  location = azurerm_resource_group.rg.location
+  name                = "capstone-nic"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name = "internal"
-    subnet_id = azurerm_subnet.subnet.id
-    public_ip_address_id = azurerm_public_ip.pip.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+
+  tags = {
+    Environment = "Capstone"
   }
 }
 
+# Associate NSG with subnet
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  subnet_id = azurerm_subnet.subnet.id
+  subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+# Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
-  name = "azure-vm"
+  name                = "azure-vm"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  size = "Standard_B1s"
-  admin_username = "azureuser"
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
 
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   admin_ssh_key {
-    username = "azureuser"
+    username   = var.admin_username
     public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
+  # Updated Ubuntu image (recommended)
   source_image_reference {
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "18.04-LTS"
-    version = "latest"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    Environment = "Capstone"
   }
 }
